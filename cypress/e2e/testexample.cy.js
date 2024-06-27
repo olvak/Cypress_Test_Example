0ba0472237818c94b2985dbda578f985bfde056d
olvak <82042917+olvak@users.noreply.github.com>
2024-06-27 15:33:42 +0300

    uncaught:exception

--- cypress/e2e/testexample.cy.js
+++ cypress/e2e/testexample.cy.js
@@ -1,6 +1,7 @@
 describe('example spec', () => {
   it('Open test page', () => {
+    cy.viewport(1366, 768)
     cy.visit('https://demoqa.com/automation-practice-form')
-    cy.contains('Practice Form').should('be.visible')
+    cy.contains('Practice Form')
   })
 })
