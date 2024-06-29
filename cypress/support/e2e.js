5e248d445ab76de0ee1de7b889585dac82defbba
olvak <82042917+olvak@users.noreply.github.com>
2024-06-29 14:40:18 +0300

    add Subjects select and disable XHR

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,4 +1,10 @@
 import './commands'
+
+before(() => {
+  // disable Cypress's default behavior of logging all XMLHttpRequests and fetches
+  cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
+})
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
